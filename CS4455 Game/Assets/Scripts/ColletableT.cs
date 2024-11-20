972ee3fcea710ef568b56cf4a1da096e00a9f2da
Charlieee0510 <zhouyuanhong0510@gmail.com>
2024-11-20 15:10:01 -0500

    Charlie fixed the collectable T script, and add time portion script

--- CS4455 Game/Assets/Scripts/ColletableT.cs
+++ CS4455 Game/Assets/Scripts/ColletableT.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ColletableT : MonoBehaviour
 {
     private TCounterUI tCounter;
+    private bool hasCollected = false;
     void Start()
     {
         // Find the TIconCounterUI script in the scene
@@ -12,9 +13,9 @@ public class ColletableT : MonoBehaviour
     }
     void OnTriggerEnter(Collider c)
     {
-        if (c.CompareTag("Player"))
+        if (c.CompareTag("Player") && !hasCollected)
         {
-
+            hasCollected = true;
             GameManager.Instance.UpdateCollectiblesUI();
             Debug.Log("hit T");
             // 销毁当前的 T
