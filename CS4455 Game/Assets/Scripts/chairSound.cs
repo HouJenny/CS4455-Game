8a4c9c8b1e9c275fd499b69b303c5ea1f374651d
Jenny Hou <143646348+HouJenny@users.noreply.github.com>
2024-11-25 12:53:13 -0500

    terrain fix scooter collider fix chair audio fix

--- CS4455 Game/Assets/Scripts/chairSound.cs
+++ CS4455 Game/Assets/Scripts/chairSound.cs
@@ -5,24 +5,17 @@ using UnityEngine;
 public class chairSound : MonoBehaviour
 {
     public AudioSource audioSource; // Reference to the Audio Source
-    public AudioClip collectionSound; // Reference to the Audio Clip
-
 
     void OnTriggerEnter(Collider other)
     {
         // Check if the player interacts with the item
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Scooter") || other.CompareTag("Player"))
         {
             Debug.Log("hit Chair");
-            audioSource.Play(); // Play the sound effect
-            GameObject tempAudio = new GameObject("TempAudio");
-            AudioSource tempSource = tempAudio.AddComponent<AudioSource>();
-
-            tempSource.clip = collectionSound;
-            tempSource.Play();
-
-            Destroy(tempAudio, collectionSound.length); // Clean up
-
+            if (!audioSource.isPlaying) {
+                audioSource.Play(); // Play the sound effect
+            }
+            
         }
     }
 }
